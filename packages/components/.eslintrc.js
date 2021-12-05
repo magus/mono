f30371db568130ea8b12ff62a858407bc953ab7b
magus <noah@iamnoah.com>
2021-12-04 19:39:05 -0800

    build(lint): 🧹simplify eslint setup

--- packages/components/.eslintrc.js
+++ packages/components/.eslintrc.js
@@ -1,45 +1,64 @@
 module.exports = {
   parser: 'babel-eslint',
-  extends: ['standard', 'standard-react', 'plugin:prettier/recommended', 'prettier/standard', 'prettier/react'],
-  env: {
-    node: true,
-  },
-  overrides: [
-    {
-      files: ['**/*.test.js'],
-      env: {
-        jest: true, // now **/*.test.js files' env has both es6 *and* jest
-      },
-      // Can't extend in overrides: https://github.com/eslint/eslint/issues/8813
-      // "extends": ["plugin:jest/recommended"]
-      plugins: ['jest'],
-      rules: {
-        'jest/no-disabled-tests': 'warn',
-        'jest/no-focused-tests': 'error',
-        'jest/no-identical-title': 'error',
-        'jest/prefer-to-have-length': 'warn',
-        'jest/valid-expect': 'error',
-      },
-    },
+  extends: [
+    // force line breaks
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
-    ecmaFeatures: {
-      legacyDecorators: true,
-      jsx: true,
-    },
-  },
-  settings: {
-    react: {
-      version: '16',
-    },
-  },
   rules: {
-    'space-before-function-paren': 0,
     'react/prop-types': 0,
-    'react/jsx-handler-names': 0,
-    'react/jsx-fragments': 0,
-    'react/no-unused-prop-types': 0,
-    'import/export': 0,
   },
 };
+
+// module.exports = {
+//   parser: 'babel-eslint',
+//   extends: [
+//     // force line breaks
+//     'standard',
+//     'standard-react',
+//     'plugin:prettier/recommended',
+//     'prettier/standard',
+//     'prettier/react',
+//   ],
+//   env: {
+//     node: true,
+//   },
+//   overrides: [
+//     {
+//       files: ['**/*.test.js'],
+//       env: {
+//         jest: true, // now **/*.test.js files' env has both es6 *and* jest
+//       },
+//       // Can't extend in overrides: https://github.com/eslint/eslint/issues/8813
+//       // "extends": ["plugin:jest/recommended"]
+//       plugins: ['jest'],
+//       rules: {
+//         'jest/no-disabled-tests': 'warn',
+//         'jest/no-focused-tests': 'error',
+//         'jest/no-identical-title': 'error',
+//         'jest/prefer-to-have-length': 'warn',
+//         'jest/valid-expect': 'error',
+//       },
+//     },
+//   ],
+//   parserOptions: {
+//     ecmaVersion: 2020,
+//     ecmaFeatures: {
+//       legacyDecorators: true,
+//       jsx: true,
+//     },
+//   },
+//   settings: {
+//     react: {
+//       version: '16',
+//     },
+//   },
+//   rules: {
+//     'space-before-function-paren': 0,
+//     'react/prop-types': 0,
+//     'react/jsx-handler-names': 0,
+//     'react/jsx-fragments': 0,
+//     'react/no-unused-prop-types': 0,
+//     'import/export': 0,
+//   },
+// };
