7bb10a2f0e620091b48a2198aed4a69964f1ee48
magus <noah@iamnoah.com>
2021-12-04 23:17:05 -0800

    magic.iamnoah.com define global __DEV__

--- sites/magic.iamnoah.com/.eslintrc.js
+++ sites/magic.iamnoah.com/.eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
-  extends: 'next/core-web-vitals',
+  extends: [
+    'next/core-web-vitals',
+    // TODO use @magusn/eslint-config-magusn
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    __DEV__: 'readonly',
+  },
 };
