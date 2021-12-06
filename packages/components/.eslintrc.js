c84d08a94dfa890772acbbf7149cd1edabf2980f
magus <noah@iamnoah.com>
2021-12-05 18:41:22 -0800

    eslint 8 + use magusn react config

--- packages/components/.eslintrc.js
+++ packages/components/.eslintrc.js
@@ -1,27 +1,6 @@
 module.exports = {
   extends: [
     // force line breaks
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
+    '@magusn/magusn/react',
   ],
-  rules: {
-    'react/prop-types': 0,
-  },
-  settings: {
-    react: {
-      version: 'detect', // React version. "detect" automatically picks the version you have installed.
-      // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
-      // default to latest and warns if missing
-      // It will default to "detect" in the future
-    },
-    componentWrapperFunctions: [
-      // The name of any function used to wrap components, e.g. Mobx `observer` function.
-      // If this isn't set, components wrapped by these functions will be skipped.
-      { property: 'styled' }, // `object` is optional
-    ],
-    linkComponents: [
-      // Components used as alternatives to <a> for linking, eg. <Link to={ url } />
-      { name: 'Link', linkAttribute: 'to' },
-    ],
-  },
 };
