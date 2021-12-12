0f598d8d0b2f7095794d54710c22318365755201
magus <noah@iamnoah.com>
2021-12-12 03:45:32 -0800

    build(commitlint): disable subject-case rule

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -3,22 +3,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'subject-case': [
-      2,
-      'always',
-      [
-        // allow any type of casing in messages
-        // e.g. feat(...): MagicAuthProvider
-        'lower-case', // default
-        'upper-case', // UPPERCASE
-        'camel-case', // camelCase
-        'kebab-case', // kebab-case
-        'pascal-case', // PascalCase
-        'sentence-case', // Sentence case
-        'snake-case', // snake_case
-        'start-case', // Start Case
-      ],
-    ],
+    'subject-case': [0],
     'type-enum': [
       2,
       'always',
