7f76ecaa0e9a1a1b30c5477157d23c201209e181
magus <noah@iamnoah.com>
2021-12-12 01:24:39 -0800

    build(commitlint): allow any subject case

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,6 +1,22 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'subject-case': [
+      2,
+      'always',
+      [
+        // allow any type of casing in messages
+        // e.g. feat(...): MagicAuthProvider
+        'lower-case', // default
+        'upper-case', // UPPERCASE
+        'camel-case', // camelCase
+        'kebab-case', // kebab-case
+        'pascal-case', // PascalCase
+        'sentence-case', // Sentence case
+        'snake-case', // snake_case
+        'start-case', // Start Case
+      ],
+    ],
     'type-enum': [
       2,
       'always',
