cbfef5359aa8020dcc53cb1345af590b767b4f7f
magus <noah@iamnoah.com>
2022-02-13 23:19:40 -0800

    feat(arceus): PokemonForm

---
+++ sites/arceus.now.sh/components/PokemonForm.js
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { TypePill } from './TypePill';
+import { PokemonImage } from './PokemonImage';
+
+export function PokemonForm(props) {
+  const [type_a, type_b] = props.form.types;
+
+  return (
+    <>
+      <h1>{props.form.name ? `${props.pokemon.name} (${props.form.name})` : props.pokemon.name}</h1>
+      <Types>
+        <TypePill type={type_a} />
+        <TypePill type={type_b} />
+      </Types>
+
+      <FormImage>
+        <PokemonImage form={props.form} pokemon={props.pokemon} type="large" />
+      </FormImage>
+
+      {props.pokemon.forms.length === 1 ? null : (
+        <>
+          <AlternateForms>
+            <div className="scroll">
+              <div className="scroll-content">
+                {props.pokemon.forms.map((form, i) => {
+                  const active = form.name === props.form.name;
+
+                  return (
+                    <SelectFormButton disabled={active} key={i} onClick={() => props.handleSelectForm(i)}>
+                      <AlternateFormImage>
+                        <PokemonImage form={form} pokemon={props.pokemon} type="small" />
+                      </AlternateFormImage>
+                      <div>{form.name || props.pokemon.name}</div>
+                    </SelectFormButton>
+                  );
+                })}
+              </div>
+            </div>
+
+            <div className="scroll-fadeLeft" />
+            <div className="scroll-fadeRight" />
+          </AlternateForms>
+        </>
+      )}
+    </>
+  );
+}
+
+const Types = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: var(--spacer);
+`;
+
+const FormImage = styled.div`
+  padding: var(--spacer) 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const AlternateForms = styled.div`
+  position: relative;
+  width: calc(100% + var(--spacer-2));
+  margin: 0 0 0 calc(-1 * var(--spacer-2));
+
+  .scroll {
+    padding: 0 var(--spacer-2);
+    overflow-x: scroll;
+    display: flex;
+  }
+
+  .scroll-content {
+    border: 1px solid rgba(var(--font-color), 0.2);
+    border-radius: var(--spacer);
+    padding: var(--spacer) var(--spacer-2);
+    display: flex;
+    flex-direction: row;
+    gap: var(--spacer-2) var(--spacer-3);
+  }
+
+  .scroll-fadeLeft {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: linear-gradient(to left, rgba(var(--background-color), 0) 0%, rgba(var(--background-color), 1) 100%);
+    width: var(--spacer-2);
+    height: 100%;
+  }
+
+  .scroll-fadeRight {
+    position: absolute;
+    top: 0;
+    right: 0;
+    background: linear-gradient(to right, rgba(var(--background-color), 0) 0%, rgba(var(--background-color), 1) 100%);
+    width: var(--spacer-2);
+    height: 100%;
+  }
+`;
+
+const SelectFormButton = styled.button`
+  border: none;
+  background: transparent;
+  color: var(--font-color);
+  opacity: ${(props) => (props.disabled ? 0.4 : 1)};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const AlternateFormImage = styled.div`
+  width: 64px;
+`;
--- sites/arceus.now.sh/components/screens/PokemonDetail.js
+++ sites/arceus.now.sh/components/screens/PokemonDetail.js
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { useRouter } from 'next/router';
-import { TypePill } from '../TypePill';
 import { Section } from '../Section';
-import { PokemonImage } from '../PokemonImage';
+import { PokemonForm } from '../PokemonForm';
 import { PokemonMoves } from '../PokemonMoves';
 import { QueryParams } from '../../src/QueryParams';
 
@@ -48,125 +47,14 @@ export function PokemonDetail(props) {
   return (
     <Container>
       <PokemonForm form={form} pokemon={pokemon} handleSelectForm={set_formIndex} />
+
+      <Section name="Moves">
+        <PokemonMoves moves={form.moves} />
+      </Section>
     </Container>
   );
 }
 
 const Container = styled.div`
   padding: var(--spacer-2);
-`;
-
-const Types = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: var(--spacer);
-`;
-
-const AlternateForms = styled.div`
-  position: relative;
-  width: calc(100% + var(--spacer-2));
-  margin: 0 0 0 calc(-1 * var(--spacer-2));
-
-  .scroll {
-    padding: 0 var(--spacer-2);
-    overflow-x: scroll;
-    display: flex;
-  }
-
-  .scroll-content {
-    border: 1px solid rgba(var(--font-color), 0.2);
-    border-radius: var(--spacer);
-    padding: var(--spacer) var(--spacer-2);
-    display: flex;
-    flex-direction: row;
-    gap: var(--spacer-2) var(--spacer-3);
-  }
-
-  .scroll-fadeLeft {
-    position: absolute;
-    top: 0;
-    left: 0;
-    background: linear-gradient(to left, rgba(var(--background-color), 0) 0%, rgba(var(--background-color), 1) 100%);
-    width: var(--spacer-2);
-    height: 100%;
-  }
-
-  .scroll-fadeRight {
-    position: absolute;
-    top: 0;
-    right: 0;
-    background: linear-gradient(to right, rgba(var(--background-color), 0) 0%, rgba(var(--background-color), 1) 100%);
-    width: var(--spacer-2);
-    height: 100%;
-  }
-`;
-
-const SelectFormButton = styled.button`
-  border: none;
-  background: transparent;
-  color: var(--font-color);
-  opacity: ${(props) => (props.disabled ? 0.4 : 1)};
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-const AlternateFormImage = styled.div`
-  width: 64px;
-`;
-
-function PokemonForm(props) {
-  const [type_a, type_b] = props.form.types;
-
-  return (
-    <div>
-      <h1>{props.form.name ? `${props.pokemon.name} (${props.form.name})` : props.pokemon.name}</h1>
-      <Types>
-        <TypePill type={type_a} />
-        <TypePill type={type_b} />
-      </Types>
-
-      <FormImage>
-        <PokemonImage form={props.form} pokemon={props.pokemon} type="large" />
-      </FormImage>
-
-      {props.pokemon.forms.length === 1 ? null : (
-        <>
-          <AlternateForms>
-            <div className="scroll">
-              <div className="scroll-content">
-                {props.pokemon.forms.map((form, i) => {
-                  const active = form.name === props.form.name;
-
-                  return (
-                    <SelectFormButton disabled={active} key={i} onClick={() => props.handleSelectForm(i)}>
-                      <AlternateFormImage>
-                        <PokemonImage form={form} pokemon={props.pokemon} type="small" />
-                      </AlternateFormImage>
-                      <div>{form.name || props.pokemon.name}</div>
-                    </SelectFormButton>
-                  );
-                })}
-              </div>
-            </div>
-
-            <div className="scroll-fadeLeft" />
-            <div className="scroll-fadeRight" />
-          </AlternateForms>
-        </>
-      )}
-
-      <Section name="Moves">
-        <PokemonMoves moves={props.form.moves} />
-      </Section>
-    </div>
-  );
-}
-
-const FormImage = styled.div`
-  padding: var(--spacer) 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 `;
