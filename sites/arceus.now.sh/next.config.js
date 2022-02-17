2febd5494688c93444645704d049d1c93a7aea21
magus <noah@iamnoah.com>
2022-02-17 02:15:28 -0800

    build(arceus): FORCE=true bypass eslint for prod

--- sites/arceus.now.sh/next.config.js
+++ sites/arceus.now.sh/next.config.js
@@ -3,4 +3,9 @@ module.exports = {
   images: {
     domains: ['www.serebii.net'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even with eslint errors
+    // > FORCE=true mono ws arceus deploy:prod
+    ignoreDuringBuilds: process.env.FORCE === 'true',
+  },
 };
