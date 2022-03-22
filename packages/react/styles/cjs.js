f4f9553903b9b9104d834992dfaa03edce79b9d2
magus <noah@iamnoah.com>
2022-03-21 21:42:28 -0700

    chore: spacing

--- packages/react/styles/cjs.js
+++ packages/react/styles/cjs.js
@@ -1,4 +1,5 @@
 // apply babel plugins to json
+
 exports.babelrc = function babelrc(json) {
   if (!json.plugins) json.plugins = [];
   json.plugins.push(['styled-components', { ssr: true, displayName: true }]);
