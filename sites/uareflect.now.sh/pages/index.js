8c27300692cb36ce8b69053cd4e52c1aa0be3973
magus <noah@iamnoah.com>
2022-06-08 19:47:27 -0700

    feat(uareflect): better meta

--- sites/uareflect.now.sh/pages/index.js
+++ sites/uareflect.now.sh/pages/index.js
@@ -2,17 +2,33 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
 export default function Home(props) {
+  const title = 'ua reflect';
+  const description = props.ua;
+  const keywords = ['user agent', 'developer', 'test', 'debug', 'tool'];
+  const image = '/ua-reflect.png';
+
   return (
     <div className={styles.container}>
       <Head>
         <title>ua reflect</title>
-        <meta name="title" content="ua reflect" />
-        <meta name="description" content={props.ua} />
+        <meta name="title" content={title} />
+        <meta name="description" content={description} />
+        <meta name="keywords" content={keywords.join(', ')} key="meta:keywords" />
+
         <link rel="icon" href="/favicon.ico" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://uareflect.vercel.app" />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={image} />
+
+        <meta property="twitter:creator" content="magusnn" />
+        <meta property="twitter:creator:id" content="23604692" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>{props.ua}</h1>
+        <code className={styles.title}>{props.ua}</code>
       </main>
     </div>
   );
