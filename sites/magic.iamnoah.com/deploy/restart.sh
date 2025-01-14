f53ebe760945ad4596e6bef8cf2b24d6d7388b2b
magus <noah@iamnoah.com>
2025-01-14 11:26:53 -0800

    feat(db): restart script stop and start

--- sites/magic.iamnoah.com/deploy/restart.sh
+++ sites/magic.iamnoah.com/deploy/restart.sh
@@ -12,4 +12,13 @@ log() {
 }
 
 log "Restarting instance"
-dokku ps:restart hasura
+
+# as of 2025-01 restart below was failing in digitalocean due to hitting resource limits
+# this meant the dokku app eventually crashed because it was never restarted periodically
+# ideally we would run restart below but it requires running two containers concurrently
+# opt to stop and start instead which is not zero downtime but reduces resource footprint
+#
+#   dokku ps:restart hasura
+#
+dokku ps:stop hasura
+dokku ps:start hasura
